28be7e49a269b0a76bd300fffab5b55944aaf84d
scm127 <67708429+scm127@users.noreply.github.com>
2021-02-23 14:09:04 -0500

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -38,7 +38,11 @@ def to_usd(my_price):
 
 
 # TODO: write some Python code here to produce the desired output
-user_input= input("Please input a user idenifier, or 'DONE' if there are no more items:")
-items = [p for p in products if str(p["id"])==str(user_input)]
-item=items[0]
-print("Selected project: " + item["name"]+" "+str(item["price"]))
+while True:   
+    user_input= input("Please input a user idenifier, or 'DONE' if there are no more items:")
+    items = [p for p in products if str(p["id"])==str(user_input)]
+    item=items[0]
+    if user_input=="DONE":
+        break
+    print("Selected project: " + item["name"]+" "+str(item["price"]))
+     
